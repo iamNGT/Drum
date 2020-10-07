a337b7f746e7c4a251442cf48161055a80dc9e38
iamNGT <guytresor1811@gmail.com>
2020-10-07 22:40:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// $(document).ready(function (){
+//     $('#btn1').click( function () {
+//         $('#sound1').ready()
+//     });
+// })
+
+
+let btn1 = document.querySelector('#btn1')
+let sound1 = document.querySelector('#sound1')
+
+btn1.addEventListener('click', function (){
+    sound1.play()
+    btn1.classList.add('btnhov')
+})
+
+let btn2 = document.querySelector('#btn2')
+let sound2 = document.querySelector('#sound2')
+
+btn2.addEventListener('click', function (){
+    sound2.play()
+    btn1.classList.add('btnhov')
+})
+
+let btn3 = document.querySelector('#btn3')
+let sound3 = document.querySelector('#sound3')
+
+btn3.addEventListener('click', function (){
+    sound3.play()
+    btn1.classList.add('btnhov')
+})
+
+let btn4 = document.querySelector('#btn4')
+let sound4 = document.querySelector('#sound4')
+
+btn4.addEventListener('click', function (){
+    sound4.play()
+    btn1.classList.add('btnhov')
+})
+
+let btn5 = document.querySelector('#btn5')
+let sound5 = document.querySelector('#sound5')
+
+btn5.addEventListener('click', function (){
+    sound5.play()
+    btn1.classList.add('btnhov')
+})
+
+let btn6 = document.querySelector('#btn6')
+let sound6 = document.querySelector('#sound6')
+
+btn6.addEventListener('click', function (){
+    sound6.play()
+    btn1.classList.add('btnhov')
+})
+
+let btn7 = document.querySelector('#btn7')
+let sound7 = document.querySelector('#sound7')
+
+btn7.addEventListener('click', function (){
+    sound7.play()
+    btn1.classList.add('btnhov')
+})
